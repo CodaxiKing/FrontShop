fa783ae459bf5617f0e29ec5c239c88fb8ee16d1
Diego Tavares <107019645+CodaxiKing@users.noreply.github.com>
2025-10-30 13:32:14 -0300

    att

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [["react-native-reanimated/plugin"]],
+    plugins: [
+      "@babel/plugin-transform-class-static-block",
+      // outros plugins que você eventualmente tenha,
+      // SEMPRE deixe o reanimated por último:
+      "react-native-reanimated/plugin",
+    ],
   };
 };
